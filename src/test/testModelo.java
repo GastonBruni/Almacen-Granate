e8c62c6d6c502d330dab4f44fc91814c107986db
Gaston <Gastonbrunijavier@hotmail.com>
2020-09-13 18:50:50 -0300

    Valido un cuit y lo agrego al testeo.

--- src/test/testModelo.java
+++ src/test/testModelo.java
@@ -9,8 +9,11 @@ public class testModelo {
 		
 		Comercio comercio = new Comercio();
 		
+		// Valido un cuit para testeo
+		comercio.setCuit(2041354026);
+		System.out.println(comercio.getCuit());
 		//Agregar XY -> Hombre 20 || Mujer 27 || Ambos 23 || Empresas 30 
-		System.out.println("Digito Verificador: "+comercio.validarIdentificadorUnico("2323523120"));
+		System.out.println("Digito Verificador: "+comercio.validarIdentificadorUnico("2041354026"));
 	}
 
 }
